9e0db6099be8c0aa4eb0c413daf6c18fc6a5e0e9
y-harada <createdirect2@gmail.com>
2020-03-30 16:25:10 +0900

    データベースの仕組み作成

---
+++ Assets/Common/Editor/AssetDatabaseEx.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEditor;
+
+public static class  AssetDatabaseEx 
+{
+	public static T[] GetAssetsFromFolder<T>(string folderPath) where T : class
+	{
+		string[] paths = Directory.GetFiles(folderPath);
+
+		List<T> list = new List<T>();
+
+		foreach(var path in paths)
+		{
+			var asset = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(path);
+
+			if(asset is T)
+			{
+				list.Add(asset as T);
+			}
+
+		}
+
+		return list.ToArray();
+	}
+}
---
+++ Assets/Common/Script/Database/Editor/ToolDatabaseInspector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+[CustomEditor(typeof(ToolDatabase))]
+public class ToolDatabaseInspector : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		if(GUILayout.Button("データセット"))
+		{
+			DatabaseInspector.Set<ToolData>("Assets/SceneData/Game/ToolData", target);
+		}
+
+		if(GUILayout.Button(("TEST")))
+		{
+			var t = (ToolDatabase)target;
+			Debug.Log(t.SearchFromId(0).Name);
+		}
+	}
+}
